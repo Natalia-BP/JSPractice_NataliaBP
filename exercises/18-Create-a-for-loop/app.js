338288a3746d5b161a792260a6e90269f5df0a80
Natalia-BP <nataliabarqueroperez@gmail.com>
2021-01-25 19:00:23 +0000

    avance hasta ejercicio 20

--- exercises/18-Create-a-for-loop/app.js
+++ exercises/18-Create-a-for-loop/app.js
@@ -1,7 +1,11 @@
 /* This exercise should be moved before 17-Your first loop? */
 
 // Declare and write your function here:
-function standardsMaker("I will write questions if I'm stuck")
+function standardsMaker() {
+    for (let standardsMaker = 0; standardsMaker < 300; standardsMaker++) {
+        console.log("I will write questions if I'm stuck")
+    }
+}
 
 // Calling the function:
 standardsMaker();
--- exercises/19-While-loop/app.js
+++ exercises/19-While-loop/app.js
@@ -3,10 +3,11 @@
 function startCounting()
 {
 	var counter = 100;
-	while(counter <= 100)
+	while(counter >= 0)
 	{
-		counter --;
-		console.log(counter);
+        console.log(counter);
+        counter = counter - 1; 
+        // Esto tiene que ir abajo del console.log porque si no, para cuando imprime en la pantalla le quita -1 y lo imprime 99 veces en vez de 100
 	}
 	
 	return counter;
