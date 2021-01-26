0ef0cacf9b45aa76cf7fbcc2c2768e45f1dc2c4b
Natalia-BP <nataliabarqueroperez@gmail.com>
2021-01-26 00:22:47 +0000

    avance a ej 23

--- exercises/22-Russian-Roulette/app.js
+++ exercises/22-Russian-Roulette/app.js
@@ -1,14 +1,16 @@
-var bulletPosition = 4;
+var bulletPosition = Math.floor((Math.random() * 5) + 1);
 
-const spinChamber = () => {
-	var chamberPosition = Math.floor((Math.random() * 6) + 1);
+let spinnerPosition = Math.floor((Math.random() * 5) + 1);
+
+const spinChamber = spinnerPosition => {
+	var chamberPosition = Math.floor((Math.random() * 5) + 1);
 	return chamberPosition;
 };
 
 // Remove the // below and complete the commented lines
-const fireGun = (spinnerPosition) => {
-    //if (...) return ("You're dead!");
-    //else return ("Keep playing!");
+const fireGun = spinnerPosition => {
+    if (bulletPosition == spinnerPosition) return ("You're dead!");
+    else return ("Keep playing!");
 };
 
-//console.log(fireGun(...));
+console.log(fireGun(spinnerPosition));
--- exercises/23-The-Beatles/app.js
+++ exercises/23-The-Beatles/app.js
@@ -1,5 +1,23 @@
+function sing() {
+    for (let i = 1; i <= 12; i++) {
+
+        if (i == 5) {
+
+            console.log("words of wisdom,")
+
+        } else if (i == 11) {
+
+            console.log("there will be an answer,")
+
+        } else {
+            console.log("let it be,")
+        }
+    }
+
+}
+
+sing()
 
 
-//Your code above ^^^
-
-console.log(sing());
+// // Your code above ^^^
+// console.log(sing());
