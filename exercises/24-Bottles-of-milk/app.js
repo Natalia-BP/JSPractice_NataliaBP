727dbb5ab5a78731f513a4455062de4f85851dc9
Natalia-BP <nataliabarqueroperez@gmail.com>
2021-01-30 18:30:11 +0000

    ej milk bottles completado correcto

--- exercises/24-Bottles-of-milk/app.js
+++ exercises/24-Bottles-of-milk/app.js
@@ -7,9 +7,9 @@ function milkSong() {
     // let bottlesLeft = `${numBottles} bottles of milk on the wall, ${numBottles} bottles of milk. Take one down and pass it around, `;
     // let oneBottle = `${numBottles} bottle of milk on the wall, ${numBottles} bottle of milk. Take one down ans pass it around, `;
     // let final = "No more bottles, no more bottles of milk. Go to the store and buy some more, ";
-    let todaEstrofa = "";
+    // let todaEstrofa = "";
 
-for (let numBottles = 99; numBottles >= 0; numBottles--) {
+for (let numBottles = 97; numBottles >= 0; numBottles--) {
     
     if (numBottles >= 2) {
 
@@ -25,6 +25,10 @@ for (let numBottles = 99; numBottles >= 0; numBottles--) {
 
         let final = "No more bottles of milk on the wall, no more bottles of milk. Go to the store and buy some more, ";
         console.log(final);
+
+    } else {
+
+        console.log(bottlesLeft);
 
     }
 
